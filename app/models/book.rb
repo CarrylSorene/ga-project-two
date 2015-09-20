4417c760b3b2133572f57c2c56a9c2f5f0c9b0df
Carryl Sorene <carrylsorene@gmail.com>
2015-09-20 16:37:10 +0100

    Installed Foundation. Removed bits of code related to Carrierwave for now

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
 
-  mount_uploader :cover, CoverUploader
+  # mount_uploader :cover, CoverUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :books, through: :reviews
 
-  mount_uploader :avatar, AvatarUploader
+  # mount_uploader :avatar, AvatarUploader
 end
